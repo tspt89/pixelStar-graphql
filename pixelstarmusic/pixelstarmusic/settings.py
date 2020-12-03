05402945a2e90668d9187a8183777fb77897e282
tspt89 <tspt89@hotmail.com>
2020-12-03 15:37:38 -0600

    users and tokens

--- pixelstarmusic/pixelstarmusic/settings.py
+++ pixelstarmusic/pixelstarmusic/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'links',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
@@ -118,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'pixelstarmusic.schema.schema',
+    #'MIDDLEWARE':  [
+    #    'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    #],
+}
--- pixelstarmusic/settings.py
+++
@@ -1,8 +0,0 @@
-INSTALLED_APPS = (
-    # After the default packages
-    'graphene_django',
-)
-
-GRAPHENE = {
-    'SCHEMA': 'hackernews.schema.schema',
-}
