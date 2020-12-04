9d4f48f6a03686d470aad5a4a806d26a3dbb1d19
tspt89 <tspt89@hotmail.com>
2020-12-04 13:49:19 -0600

    se agrega redis mostrarCanciones

--- pixelstarmusic/pixelstarmusic/urls.py
+++ pixelstarmusic/pixelstarmusic/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+from mostrarPlaylist.views import redis
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('redis',redis),
 ]
